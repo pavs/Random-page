0088c26faeb3faf69283297b5d3e87602e0b4026
Pavel Mah <pavs.ma@hotmail.com>
2013-07-28 21:29:02 +0600

    Random Quotes Generator

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+<link rel="stylesheet" type="text/css" href="./css/screen.css">
+
+</head>
+<body>
+
+</body>
+</html>
+
+
+<?php
+
+$page = include('pages/' . rand(1, 10) . '.html');
+
+echo $pages;
+
+?>
